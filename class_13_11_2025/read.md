23f123caf7e1849ca2f15ab85c0c8ced9e331949
Arpit000009 <100606591+Arpit000009@users.noreply.github.com>
2025-11-13 10:17:58 +0530

    BOM

---
+++ class_13_11_2025/read.md
@@ -0,0 +1,5 @@
+DOM
+document object model
+
+DOM is a tree like heirarichial structure which represent html documnet
+DOM is a part of browser
---
+++ class_13_11_2025/script.js
@@ -0,0 +1,16 @@
+//! open
+//open is used to open a new tab with the 
+// open("https://www.amazon.com")
+// open("https://www.filpkart.com")
+
+// close()
+// !6.setTime(()={
+// },delay)
+
+//! it ia uaed to perform some task asynchronously after  a particular delay
+//it will be executed once
+
+
+let inter = setIterval(()=>{
+
+});
